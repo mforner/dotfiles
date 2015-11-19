6f3fa9dfd524c3d44e10ede3eb93f6f88e015a3e
Mario Forner <mail@marioforner.de>
2015-11-19 06:37:39 +0100

    Add oh-my-zsh as submodule

--- symlinks.sh
+++ symlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc vimrc wallpaper.jph i3 fonts"    # list of files/folders to symlink in homedir
+files="zshrc vimrc wallpaper.jph i3 fonts oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
